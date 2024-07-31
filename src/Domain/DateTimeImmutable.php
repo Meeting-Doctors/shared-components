b1365c351e9e4149501d073af1a67fc882498be5
javier.rodriguez <javier.rodriguez@meetingdoctors.com>
2024-07-31 20:08:30 +0200

    Refactor class

--- src/Domain/DateTimeImmutable.php
+++ src/Domain/DateTimeImmutable.php
@@ -6,30 +6,64 @@ namespace Shared\Domain;
 
 use Assert\Assertion;
 
+use DateTimeImmutable as NativeDateTimeImmutable;
+use InvalidArgumentException;
+
 final readonly class DateTimeImmutable
 {
-    public string $dateTime;
+    private const string DATE_FORMAT = 'U';
 
-    public function __construct(
-        string $dateTime
+    private int $timestamp;
+
+    private function __construct(
+        int $timestamp
     ) {
-        Assertion::date($dateTime, DATE_ATOM);
-
-        $this->dateTime = $dateTime;
+        $this->timestamp = $timestamp;
     }
 
     public static function now(): self
     {
-        return new self(date(DATE_ATOM));
+        $date = new NativeDateTimeImmutable();
+
+        return new self($date->getTimestamp());
+    }
+
+    public static function fromFormat(string $format, $datetime): self
+    {
+        $date = NativeDateTimeImmutable::createFromFormat($format, $datetime);
+
+        if ($date === false) {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Invalid datetime format "%s" for "%s"',
+                    $format,
+                    $datetime
+                )
+            );
+        }
+
+        return new self($date->getTimestamp());
+    }
+
+    public function timestamp(): int
+    {
+        return $this->timestamp;
+    }
+
+    public function format(string $format): string
+    {
+        $date = NativeDateTimeImmutable::createFromFormat(self::DATE_FORMAT, (string) $this->timestamp);
+
+        return $date->format($format);
     }
 
     public function equals(DateTimeImmutable $dateTime): bool
     {
-        return $this->dateTime === $dateTime->dateTime;
+        return $this->timestamp() === $dateTime->timestamp();
     }
 
     public function gt(DateTimeImmutable $dateTime): bool
     {
-        return $this->dateTime > $dateTime->dateTime;
+        return $this->timestamp > $dateTime->timestamp;
     }
 }
--- tests/Domain/DateTimeImmutableTest.php
+++ tests/Domain/DateTimeImmutableTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Shared\Tests\Domain;
 
 use Assert\AssertionFailedException;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 use Shared\Domain\DateTimeImmutable;
 
@@ -12,15 +13,15 @@ final class DateTimeImmutableTest extends TestCase
 {
     public function test_must_throw_assertion_failed_exception(): void
     {
-        self::expectException(AssertionFailedException::class);
+        self::expectException(InvalidArgumentException::class);
 
-        new DateTimeImmutable('date');
+        DateTimeImmutable::fromFormat('u', '');
     }
 
     public function test_must_compare_two_instances(): void
     {
-        $some = new DateTimeImmutable('2024-01-01T00:00:00+00:00');
-        $another = new DateTimeImmutable('2024-01-01T00:00:00+01:00');
+        $some = DateTimeImmutable::fromFormat(DATE_ATOM, '2024-01-01T00:00:00+00:00');
+        $another = DateTimeImmutable::fromFormat(DATE_ATOM, '2024-01-01T00:00:00+01:00');
 
         self::assertFalse($some->equals($another));
     }
